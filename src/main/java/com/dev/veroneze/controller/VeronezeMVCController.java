f71be472c28d06c0833c433d542ac973546891ea
Lucas Carlos Cardoso de Loiola <lukas@TI.veroneze.local>
2023-11-27 16:51:33 -0300

    implementing the Controller on the 'index' and 'product' pages

---
+++ src/main/java/com/dev/veroneze/controller/VeronezeMVCController.java
@@ -0,0 +1,14 @@
+package com.dev.veroneze.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VeronezeMVCController {
+    
+    @GetMapping("/")
+    public String viewHomePage() {
+        return "index";
+    }
+}
